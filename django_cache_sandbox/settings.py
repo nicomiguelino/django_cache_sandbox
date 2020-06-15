9f932a08c34ab88f417e880e612bcd8e97638f7c
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-15 17:41:48 +0800

    Added support for local memory caching

--- django_cache_sandbox/settings.py
+++ django_cache_sandbox/settings.py
@@ -115,6 +115,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Caching
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+    }
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
